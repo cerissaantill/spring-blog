d89e9709e7db56302a17120cef4526328f9f71c4
Cerissa Antill <cerissa.antill@gmail.com>
2019-05-02 14:51:06 -0500

    completed PostController

--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -5,12 +5,40 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.lang.String;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
+
+@Controller
 public class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String showPosts(){
+        return "posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String showPost(@PathVariable long id){
+        return "view an individual post id: " + id;
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String showCreateForm(){
+        return "view the form for creating a post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPost(){
+        return "create a new post";
+    }
+
+}
 
 
     
 
-}
+
