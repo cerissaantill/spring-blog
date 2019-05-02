1686a37f98c4862e8aed263e5c3a6a37a7951eaa
Cerissa Antill <cerissa.antill@gmail.com>
2019-05-02 14:48:46 -0500

    finished MathController, starting DiceControllerExercise

---
+++ src/main/java/com/codeup/blog/DiceController.java
@@ -0,0 +1,31 @@
+package com.codeup.blog;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@Controller
+public class DiceController {
+
+    // roll-dice page:
+    @GetMapping("/roll-dice")
+    public String rollTheDice() {
+        return "rollDice";
+    }
+
+    // roll-dice results page:
+    @GetMapping("/roll-dice/{guess}")
+    public String rollTheDice(@PathVariable int guess, Model model) {
+        System.out.println(guess);
+        int random = (int) Math.ceil(Math.random() * 6);
+        model.addAttribute("random", random);
+        model.addAttribute("guess", guess);
+
+    }
+}
+
+
