cc190262a941e1d1c365fbd3ee9857bb527b68a6
Cerissa Antill <cerissa.antill@gmail.com>
2019-05-02 11:57:22 -0500

    spring blog setup

---
+++ src/main/java/com/codeup/blog/MathController.java
@@ -0,0 +1,46 @@
+package com.codeup.blog;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.lang.String;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+    @Controller
+    public class MathController {
+
+        @GetMapping("/add/{input1}/and/{input2}")
+        @ResponseBody
+        public String add(@PathVariable int input1,@PathVariable int input2){
+            return String.valueOf(input1 + input2);
+        }
+
+
+        @GetMapping("/sub/{input2}/from/{input1}")
+        @ResponseBody
+        public String sub(@PathVariable int input1,@PathVariable int input2){
+            return String.valueOf(input1 - input2);
+        }
+
+
+        @GetMapping("/multi/{input1}/and/{input2}")
+        @ResponseBody
+        public String multi(@PathVariable int input1,@PathVariable int input2){
+            return String.valueOf(input1 * input2);
+        }
+
+
+        @GetMapping("/div/{input1}/by/{input2}")
+        @ResponseBody
+        public String div(@PathVariable int input1,@PathVariable int input2){
+            return String.valueOf(input1/input2);
+        }
+
+    }  // MathController class
+
+
+
+
---
+++ src/main/java/com/codeup/blog/PostController.java
@@ -0,0 +1,16 @@
+package com.codeup.blog;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.lang.String;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+public class PostController {
+
+
+    
+
+}
