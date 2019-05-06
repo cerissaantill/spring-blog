3e8449091f120f3c3a39472896cc1a0f696bfb93
Cerissa Antill <cerissa.antill@gmail.com>
2019-05-06 11:24:02 -0500

    added Property classes

---
+++ src/main/java/com/codeup/blog/Property.java
@@ -0,0 +1,36 @@
+package com.codeup.blog;
+
+import javax.persistence.*;
+
+
+
+
+@Entity
+public class Property {
+
+
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, length = 100)
+    private String address;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(name="asking_price")
+    private int askingPrice;
+
+    @Column
+    private int bedrooms;
+
+    @Column
+    private double bathrooms;
+
+    @Column(name="square_footage")
+    private int sqareFootage;
+
+
+}
---
+++ src/main/java/com/codeup/blog/PropertyRepository.java
@@ -0,0 +1,7 @@
+package com.codeup.blog;
+
+//import org.springframework.data.repository.CrudRepository;
+
+
+public class PropertyRepository {
+}
