5e92b646b79ab5f3a2e844ebeef4c0d704876800
Cerissa Antill <cerissa.antill@gmail.com>
2019-05-13 07:49:25 -0500

    minor changes to services/ UserDetailsLoader.java

--- src/main/java/com/codeup/blog/controllers/MathController.java
+++ src/main/java/com/codeup/blog/controllers/MathController.java
@@ -14,28 +14,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 
         @GetMapping("/add/{input1}/and/{input2}")
         @ResponseBody
-        public String add(@PathVariable int input1,@PathVariable int input2){
+        public String add(@PathVariable int input1, @PathVariable int input2){
             return String.valueOf(input1 + input2);
         }
 
 
         @GetMapping("/sub/{input2}/from/{input1}")
         @ResponseBody
-        public String sub(@PathVariable int input1,@PathVariable int input2){
+        public String sub(@PathVariable int input1, @PathVariable int input2){
             return String.valueOf(input1 - input2);
         }
 
 
         @GetMapping("/multi/{input1}/and/{input2}")
         @ResponseBody
-        public String multi(@PathVariable int input1,@PathVariable int input2){
+        public String multi(@PathVariable int input1, @PathVariable int input2){
             return String.valueOf(input1 * input2);
         }
 
 
         @GetMapping("/div/{input1}/by/{input2}")
         @ResponseBody
-        public String div(@PathVariable int input1,@PathVariable int input2){
+        public String div(@PathVariable int input1, @PathVariable int input2){
             return String.valueOf(input1/input2);
         }
 
--- src/main/java/com/codeup/blog/services/UserDetailsLoader.java
+++ src/main/java/com/codeup/blog/services/UserDetailsLoader.java
@@ -1,26 +1,28 @@
 package com.codeup.blog.services;
 
-
+import com.codeup.blog.Users;
 import com.codeup.blog.models.User;
 import com.codeup.blog.models.UserWithRoles;
 import com.codeup.blog.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 
 
 @Service
-public class UserDetailsLoader implements UserDetailService {
+public class UserDetailsLoader implements UserDetailsService {
+    private final User users;
 
-    private final UserRepository users;
-
-    public UserDetailsLoader(UserRepository users) {
+    public UserDetailsLoader(Users users) {
         this.users = users;
     }
 
 
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetailsLoader loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = users.findByUsername(username);
         if (user == null) {
             throw new UsernameNotFoundException("No user found for " + username);
